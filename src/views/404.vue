f4d9721650d2334e969810ab9291bd1529d62095
Rokneer <rokneer@gmail.com>
2023-11-20 02:43:29 -0500

    Added transitions for 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="text-center text-6xl">
-    <div class="py-6 text-9xl font-extrabold">¡404!</div>
-    <div class="py-3">Woops! Couldnt find the page you were looking for.</div>
-    <div class="py-3">
+  <div class="py-4 text-center text-6xl">
+    <div
+      class="animate__animated animate__fadeInDown py-6 text-9xl font-extrabold"
+    >
+      ¡404!
+    </div>
+    <div class="animate__animated animate__fadeInDown py-3">
+      Woops! Couldnt find the page you were looking for.
+    </div>
+    <div class="animate__animated animate__fadeInDown py-3">
       How about we go back
       <router-link class="text-primary-200" :to="{ name: 'Home' }">
         home?
@@ -11,5 +17,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
