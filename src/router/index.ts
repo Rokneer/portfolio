3d8003ecf9a1ae44edd9701bd36ce8a43f6e9f1e
Rokneer <rokneer@gmail.com>
2023-11-26 15:48:50 -0500

    Added 404.html

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Portfolio from "../views/Portfolio.vue";
 import About from "../views/About.vue";
@@ -64,7 +64,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes,
   scrollBehavior() {
     return { top: 0 };
